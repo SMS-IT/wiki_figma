854e7ecc53c51595daf08f48cb705456fb43a5f5
quorse <alexej666@mail.ru>
2019-05-16 16:52:02 +0400

    first commit

---
+++ modules/ve-figma/ve.ce.MWfigmaNode.js
@@ -0,0 +1,72 @@
+/**
+ * ContentEditable MediaWiki Figma node.
+ *
+ * @class
+ * @extends ve.ce.MWInlineExtensionNode
+ *
+ * @constructor
+ * @param {ve.dm.MWFigmaNode} model Model to observe
+ * @param {Object} [config] Configuration options
+ */
+ 
+ve.ce.MWFigmaNode = function VeCeMWFigmaNode() {
+	ve.ce.MWFigmaNode.super.apply( this, arguments ); 
+};
+
+/* Inheritance */
+
+OO.inheritClass( ve.ce.MWFigmaNode, ve.ce.MWInlineExtensionNode );
+
+/* Static Properties */
+
+ve.ce.MWFigmaNode.static.name = 'mwFigma';
+
+ve.ce.MWFigmaNode.static.primaryCommandName = 'figma';
+
+ve.ce.MWFigmaNode.static.tagName = 'div';
+
+
+/* Methods */
+
+// Inherits from ve.ce.BranchNode
+ve.ce.MWFigmaNode.prototype.onSetup = function () {
+	// Parent method
+	ve.ce.MWFigmaNode.super.prototype.onSetup.call( this );
+
+	// DOM changes
+	this.$element.addClass( 've-ce-mwFigmaNode' );
+};
+
+// Inherits from ve.ce.GeneratedContentNode
+/*
+ve.ce.MWFigmaNode.prototype.generateContents = function () {
+ console.log(' !!!!!!  MWFigmaNodegenerateContents');
+	var node = this,
+		args = arguments;
+	// Parent method
+	return ve.ce.MWExtensionNode.prototype.generateContents.apply( node, args );
+};
+*/
+
+/*
+ve.ce.MWFigmaNode.prototype.validateGeneratedContents = function ( $element ) {
+	if ( $element.is( 'div' ) && $element.hasClass( 'errorbox' ) ) {
+		return false;
+	}
+	return true;
+};
+*/
+
+// Inherits from ve.ce.FocusableNode
+/*
+ve.ce.MWFigmaNode.prototype.getBoundingRect = function () {
+ console.log('ve.ce.MWFigmaNode.prototype.getBoundingRect');
+	// HACK: Because nodes can overflow due to the pre tag, just use the
+	// first rect (of the wrapper div) for placing the context.
+	return this.rects[ 0 ];
+};
+*/
+
+/* Registration */
+
+ve.ce.nodeFactory.register( ve.ce.MWFigmaNode );
